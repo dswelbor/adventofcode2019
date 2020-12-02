e58384cdb130a30b19cfd38c52ed38729d20eae8
David Welborn <dswelbor@asu.edu>
2020-12-01 23:36:50 -0500

    Adjust git url in setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/dswelbor/aoc2019',
+    url='https://github.com/dswelbor/adventofcode',
     version='1.2.0',
     zip_safe=False,
 )
